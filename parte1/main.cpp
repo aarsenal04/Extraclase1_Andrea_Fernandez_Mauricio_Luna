5af6e395dbe6904ad32232b88ec647030b275c1f
MauroCR52 <mauluna52@gmail.com>
2023-03-09 14:40:54 -0600

    Funciones de lista

--- parte1/main.cpp
+++ parte1/main.cpp
@@ -1,85 +1,195 @@
+// C++ program for the above approach
 #include <iostream>
-#include <ctime>
-#include <list>
-
-
 using namespace std;
 
+// Node class to represent
+// a node of the linked list.
 class Node {
-private:
-    int value;
-    Node* next;
-
 public:
-    Node(int val) : value(val), next(nullptr) {}
-    ~Node() {}
+    int data;
+    Node* next; // puntero al siguiente nodo
 
-    int getValue() const {
-        return value;
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
     }
 
-    Node* getNext() const {
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    int getData() const {
+        return data;
+    }
+
+    void setData(int data) {
+        Node::data = data;
+    }
+
+    Node *getNext() const {
         return next;
     }
 
-    void setNext(Node* node) {
-        next = node;
+    void setNext(Node *next) {
+        Node::next = next;
     }
 
-    // Sobrecarga de operador new
     void* operator new(size_t size);
 
-    // Sobrecarga de operador delete
-    void operator delete(void* ptr);
+    void operator delete (void* ptr);
+
+
 };
-class List {
-private:
+
+// Linked list class to
+// implement a linked list.
+class Linkedlist {
     Node* head;
 
 public:
-    List() : head(nullptr) {}
-    ~List() {}
 
-    Node* getHead() const {
-        return head;
-    }
+    Linkedlist() { head = NULL; }
 
-    void setHead(Node* node) {
-        head = node;
-    }
+    void insertNode(int);
 
-    void insert(int val) {
-        Node* node = new Node(val);
-        node->setNext(head);
-        head = node;
-    }
+    Node *getHead() const;
+
+    void setHead(Node *head);
+
+    void printList();
+
+    void deleteNode(int);
 };
-class Collector {
-private:
-    std::list<void*> memory_pool;
 
-public:
-    Collector() : memory_pool() {}
-    ~Collector() {}
+void Linkedlist::deleteNode(int nodeOffset)
+{
+    Node *temp1 = head, *temp2 = NULL;
+    int ListLen = 0;
 
-    void* allocate(size_t size) {
-        if (memory_pool.empty()) {
-            return ::operator new(size);
-        } else {
-            void* ptr = memory_pool.front();
-            memory_pool.pop_front();
-            return ptr;
-        }
+    if (head == NULL) {
+        cout << "List empty." << endl;
+        return;
     }
 
-    void deallocate(void* ptr) {
-        memory_pool.push_back(ptr);
+    // Find length of the linked-list.
+    while (temp1 != NULL) {
+        temp1 = temp1->next;
+        ListLen++;
     }
 
-    void printMemoryPool() {
-        std::cout << "Memory pool:" << std::endl;
-        for (void* ptr : memory_pool) {
-            std::cout << "  " << ptr << std::endl;
-        }
+    // Check if the position to be
+    // deleted is greater than the length
+    // of the linked list.
+    if (ListLen < nodeOffset) {
+        cout << "Index out of range"
+             << endl;
+        return;
     }
-};
+
+    // Declare temp1
+    temp1 = head;
+
+    // Deleting the head.
+    if (nodeOffset == 1) {
+
+        // Update head
+        head = head->next;
+        delete temp1;
+        return;
+    }
+
+    // Traverse the list to
+    // find the node to be deleted.
+    while (nodeOffset-- > 1) {
+
+        // Update temp2
+        temp2 = temp1;
+
+        // Update temp1
+        temp1 = temp1->next;
+    }
+
+    // Change the next pointer
+    // of the previous node.
+    temp2->next = temp1->next;
+
+    // Delete the node
+    delete temp1;
+}
+
+// Function to insert a new node.
+void Linkedlist::insertNode(int data)
+{
+    // Create the new Node.
+    Node* newNode = new Node(data);
+
+    // Assign to head
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    // Traverse till end of list
+    Node* temp = head;
+    while (temp->next != NULL) {
+
+        // Update temp
+        temp = temp->next;
+    }
+
+    // Insert at the last.
+    temp->next = newNode;
+}
+
+// Function to print the
+// nodes of the linked list.
+void Linkedlist::printList()
+{
+    Node* temp = head;
+
+    // Check for empty list.
+    if (head == NULL) {
+        cout << "List empty" << endl;
+        return;
+    }
+
+    // Traverse the list.
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *Linkedlist::getHead() const {
+    return head;
+}
+
+void Linkedlist::setHead(Node *head) {
+    Linkedlist::head = head;
+}
+
+// Driver Code
+int main()
+{
+    Linkedlist list;
+
+    // Inserting nodes
+    list.insertNode(1);
+    list.insertNode(5);
+
+    cout << "Elements of the list are: ";
+
+    // Print the list
+    list.printList();
+    cout << endl;
+
+    // Delete node at position 2.
+    //  list.deleteNode(2);
+    cout << list.getHead() << endl;
+    return 0;
+}
